2d72284eae6c1a4811a26432d446ec6c344c9589
Sixto Obreque <sixto.obreque@gmail.com>
2024-01-28 17:12:53 -0300

    se cambio codigos

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,37 @@
 // buscar los elementos
-const divA = document.querySelector('#a')
-const divB = document.querySelector('#b')
-const divC = document.querySelector('#c')
-const divD = document.querySelector('#d')
-const keyDiv = document.querySelector('key')
+//const divA = document.querySelector('#a')
+//const divB = document.querySelector('#b')
+//const divC = document.querySelector('#c')
+//const divD = document.querySelector('#d')
+//const keyDiv = document.querySelector('key')
 
 // funciones para que cambie de color
 
-const changeBackgrounColor = (event) => {
-    const color = ["purple", "blue", "green", "orange", "yellow", "red"];
+//const changeBackgrounColor = (event) => {
+    //const color = ["purple", "blue", "green", "orange", "yellow", "red"];
 
-    if (event.target.style.backgroundColor != "black") {
-        event.target.style.backgroundColor = "black"
-        return
-    }
-    const randomColor = color[Math.floor(Math.random() * color.length)]
-    event.target.style.backgroundColor = randomColor
-}
+    //if (event.target.style.backgroundColor != "black") {
+        //event.target.style.backgroundColor = "black"
+        //return
+   //}
+   // const randomColor = color[Math.floor(Math.random() * color.length)]
+    //event.target.style.backgroundColor = randomColor
+//}
 
 //Ejecutar las funciones
-divA.addEventListener("click", changeBackgrounColor)
-divB.addEventListener("click", changeBackgrounColor)
-divC.addEventListener("click", changeBackgrounColor)
-divD.addEventListener("click", changeBackgrounColor)
+//divA.addEventListener("click", changeBackgrounColor)
+////divB.addEventListener("click", changeBackgrounColor)
+//divC.addEventListener("click", changeBackgrounColor)
+//divD.addEventListener("click", changeBackgrounColor)
 
-const createNewDiv = (color) => {
-    const newDiv = document.createElement("div")
-    newDiv.style.width = "200px";
-    newDiv.style.height = "200px";
-    newDiv.style.margin = "20px";
-    newDiv.style.backgroundColor = color;
-    document.body.appendChild(newDiv);
-};
+//const createNewDiv = (color) => {
+    //const newDiv = document.createElement("div")
+    //newDiv.style.width = "200px";
+    //newDiv.style.height = "200px";
+    //newDiv.style.margin = "20px";
+   // newDiv.style.backgroundColor = color;
+   // document.body.appendChild(newDiv);
+//};
 
 //document.addEventListener("keydown", function (event) {
 // var keyDiv = document.getElementById("keyDiv");
@@ -52,32 +52,43 @@ const createNewDiv = (color) => {
 //});
 
 
-document.addEventListener("keydown", (event) => {
-    const keyDiv = document.querySelector("key")
+document.addEventListener("keydown", function(event) {
+    const key = event.keytolowerCase();
 
-    switch (event.key) {
+    let newColor;
+    switch (key) {
         case 'a':
-            keyDiv.style.backgroundColor = "pink"
+            newColor = "pink";
             break;
         case 's':
-            keyDiv.style.backgroundColor = "orange"
+            newColor = "orange";
             break;
         case 'd':
-            keyDiv.style.backgroundColor = "skyblue"
+            newColor = "skyblue";
             break;
         case 'q':
-            createNewDiv("purple")
+            newColor = ("purple");
             break;
         case 'w':
-            createNewDiv("gray")
+            newColor = ("gray");
             break;
         case 'e':
-            createNewDiv("brown")
+            newColor = ("brown");
             break;
         default:
-            break;
+            return;
     }
-})
+    const keyDiv = document.getElementById("key");
+    keyDiv.style.backgroundColor = newColor;
+});
+
+const divs = document.querySelectorAll("div");
+for (let i = 0; i < divs.length; i++){
+    divs[i].addEventListener("click", function (event){
+        event.target.style.backgroundColor = "black";
+
+    });
+}
 
 
 
